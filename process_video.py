9fdf264f6239999d310c7580fc0b904d0f040ca7
mowoe <git@mowoe.com>
2018-10-28 11:24:20 +0100

    updated the video reader (to webcam)

--- process_video.py
+++ process_video.py
@@ -2,24 +2,20 @@ import cv2
 import numpy as np
 from detect import model, display_instances, class_names
 
-capture = cv2.VideoCapture('videofile.mp4')
+capture = cv2.VideoCapture(0)
 size = (
     int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
     int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 )
-codec = cv2.VideoWriter_fourcc(*'DIVX')
-output = cv2.VideoWriter('videofile_masked.avi', codec, 60.0, size)
 
 while(capture.isOpened()):
     ret, frame = capture.read()
     if ret:
-        # add mask to frame
         results = model.detect([frame], verbose=0)
         r = results[0]
         frame = display_instances(
             frame, r['rois'], r['masks'], r['class_ids'], class_names, r['scores']
         )
-        output.write(frame)
         cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
